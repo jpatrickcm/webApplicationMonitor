e2e2d262cae149012dd1bef5b6ebcb275dacff4f
jpatrick <jeanpatrick.94@gmail.com>
2019-06-12 07:10:40 -0500

    Avance inicial, login en curso

--- WebApplication/App_Start/RouteConfig.cs
+++ WebApplication/App_Start/RouteConfig.cs
@@ -13,11 +13,24 @@ namespace WebApplication
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Login",
+                url: "login/",
+                defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
+            );
+
         }
     }
 }
---
+++ WebApplication/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Login login)
+        {
+            return View();
+        }
+    }
+}
